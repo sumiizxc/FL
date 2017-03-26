20bf922f5585a0815c71561dc7964371e79386c6
sumiizxc <ch.sumyadorj@gmail.com>
2017-03-26 15:38:45 +0900

    Update seminar2(tarhaltiin tuhai).R

--- seminar2(tarhaltiin tuhai).R
+++ seminar2(tarhaltiin tuhai).R
@@ -2,7 +2,7 @@ hist(rnorm(n6 = 15, mean = 1, sd = 2))
 pnorm(q= 1, mean= 0, sd = 1, lower.tail = FALSE) # lower.tail parametr ni zuun talaas tootsoh eswel baruun talaas tootsoh eshiig bodno :P 
 dnorm(x = 0.51, mean = 0 , sd= 1)
 round(dnorm( x = seq(from = 0, to =5.09, by = 0.01), mean = 0, sd = 1), digits = 6)
-#round function is used countable number digits added
+#round function is added countable digits of numbers.
 x = matrix(data = round(
   dnorm(x = seq(from = 0,
                 to =5.09, 
